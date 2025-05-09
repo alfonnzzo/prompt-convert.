76ba84f543ab791e184fbeaa8a335e965b7456f7
alfonnzzo <alfonsozagana@gmail.com>
2025-05-09 12:51:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const tasaCambio = 1100;
+
+let continuar = true;
+
+while (continuar) {
+const opcion = prompt(
+    "Conversor de Divisas ARS a USD\n\n" +
+    "1. Convertir ARS a USD\n" +
+    "2. Salir\n" +
+    "Ingrese una opción (1-2):"
+);
+
+if (opcion == "1") {
+    let montoARS = parseFloat(prompt("Ingrese la cantidad de ARS a convertir:"));
+    monto = parseFloat(montoARS / tasaCambio).toFixed(2);
+    alert("La cantidad de USD es: " + monto);
+} else if (opcion == "2") {
+    alert("Saliendo del conversor de divisas.");
+    continuar = false;
+} else {
+    alert("Opción no válida. Por favor, elija una opción válida.");
+}
+} 
